80d5a52b41212c5f2a5025ad726b5c2d3db1848f
Peter Moroz <ptr.moroz@gmail.com>
2023-01-12 21:03:21 +0300

    lab2: prepare libpi

--- libpi/put-your-src-here.mk
+++ libpi/put-your-src-here.mk
@@ -1,5 +1,4 @@
 # if you write a new file "foo.c" or "foo.S",  add it here as a "foo.o" 
 # (the makefile will figure out how to produce it).
 
-# uncomment gpoi.o 
-YOUR_OBJS = # gpio.o
+YOUR_OBJS = gpio.o
