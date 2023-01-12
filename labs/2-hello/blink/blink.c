0e887c0e5b1a64dafe0cbbf02eacc50372333c44
Peter Moroz <ptr.moroz@gmail.com>
2023-01-12 21:04:31 +0300

    lab2: make sure that blink still works

---
+++ labs/2-hello/blink/blink.c
@@ -0,0 +1,61 @@
+/*
+ * write code to allow blinking using arbitrary pins.    
+ * Implement:
+ *	- gpio_set_output(pin) --- set GPIO <pin> as an output (vs input) pin.
+ *	- gpio_set_on(pin) --- set the GPIO <pin> on.
+ * 	- gpio_set_off(pin) --- set the GPIO <pin> off.
+ * Use the minimal number of loads and stores to GPIO memory.  
+ *
+ * start.s defines a of helper functions (feel free to look at the assembly!  it's
+ *  not tricky):
+ *      uint32_t get32(volatile uint32_t *addr) 
+ *              --- return the 32-bit value held at <addr>.
+ *
+ *      void put32(volatile uint32_t *addr, uint32_t v) 
+ *              -- write the 32-bit quantity <v> to <addr>
+ * 
+ * Check-off:
+ *  1. get a single LED to blink.
+ *  2. attach an LED to pin 19 and another to pin 20 and blink in opposite order (i.e.,
+ *     one should be on, while the other is off).   Note, if they behave weirdly, look
+ *     carefully at the wording for GPIO set.
+ */
+
+
+#include "rpi.h"
+
+// countdown 'ticks' cycles; the asm probably isn't necessary.
+void delay(unsigned ticks) {
+    while(ticks-- > 0)
+        asm("add r1, r1, #0");
+}
+
+// when you run should blink 10 times. will have to restart the pi by pulling the
+// usb connection out.
+void notmain(void) {
+    int led = 20;
+    int but = 21;
+    gpio_set_output(led);
+    gpio_set_input(but);
+    for (int i = 0; i < 10; i++) {
+        gpio_set_on(led);
+        delay(1000000);
+        gpio_set_off(led);
+        delay(1000000);
+    }
+
+    unsigned cnt = 0;
+
+    while (cnt < 10) {
+        if (gpio_read(but)) {
+            gpio_set_on(led);
+            cnt += 1;
+        }
+        else
+            gpio_set_off(led);
+        delay(1000000);
+    }
+
+    clean_reboot();
+}
+
