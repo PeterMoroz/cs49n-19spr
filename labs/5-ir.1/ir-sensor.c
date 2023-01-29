91f0944ff353707b50cc50732c222cadbc9c0317
Peter Moroz <ptr.moroz@gmail.com>
2023-01-29 20:43:57 +0300

    lab5: a simple tracer to reverse engineering IR remote control protocols

--- labs/5-ir.1/ir-sensor.c
+++ labs/5-ir.1/ir-sensor.c
@@ -22,6 +22,11 @@ int read_while_eq(int pin, int v, unsigned timeout) {
 }
 
 
+ struct
+ {
+  unsigned v;
+  unsigned dt;
+ } sequence[96];
 
 
 void notmain(void) {
@@ -32,9 +37,55 @@ void notmain(void) {
     gpio_set_pullup(PIN);
     printk("sensor ready!\n");
 
+    
+
     while (1) {
+      /*
       uint32_t p = gpio_read(PIN);
       printk("pin = %u\n", p);
+      */
+      int rc = read_while_eq(PIN, 1, timeout);
+      if (rc == 0)  // expired timeout
+        continue;
+
+
+      size_t idx = 0;
+      unsigned val = 0;
+      while (1) {
+        if (val == 0) {
+          unsigned t = timer_get_usec();
+          while (gpio_read(PIN) == val) ;
+          unsigned dt = timer_get_usec() - t;
+          sequence[idx].v = val;
+          sequence[idx].dt = dt;
+          val = 1;
+          idx++;
+        } else {
+          unsigned t = timer_get_usec();
+          unsigned dt = t;          
+          while ((dt = timer_get_usec() - t) < 10000) { // read but no longer than 10 ms
+            if (gpio_read(PIN) != val) {
+              break;
+            }
+          }
+
+          if (dt < 10000) {
+            sequence[idx].v = val;
+            sequence[idx].dt = dt;
+            val = 0;
+            idx++;
+          } else {
+            break;
+          }
+          
+        }
+      }
+
+      for (size_t i = 0; i < idx; i++) {
+        printk("%02u - %u: %03u us\n", i, sequence[i].v, sequence[i].dt);
+      }
+      printk("--------------------\n");
+      
       delay_ms(1000);
     }
 
